d73f6e7e551d95104124008bcc04048335b9237f
abelisanalyn <analyn112394@gmail.com>
2025-08-13 02:10:26 +0000

    ACT 2

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+$Name = "Analyn Abelis";
+$Age = "30";
+$School = "Global Reciprocal College";
+$Course = "BSIT";
+$Subject = "WEBSYSL";
+$Student = "YES";
+echo "Name " . $Name . "<br>";
+echo "Age: " . $Age . "<br>";
+echo "School: " . $School . "<br>";
+echo "Course: " . $Course . "<br>";
+echo "Subject: " . $Subject . "<br>";
+echo "Student: " .  Var_dump($Student) . "<br>";
+?>
